c94af3a28786bcd86f4c3fcf03f6cc9564205fbc
= <pezi10002@hotmail.com>
2023-02-10 23:48:24 +0100

    schema bug fixed, readme updated with api-docs

---
+++ utils/create_swagger_yaml_file.py
@@ -0,0 +1,12 @@
+# import requests
+# import yaml
+
+# # Make a GET request to an endpoint in your Django application
+# response = requests.get("http://localhost:8000/schema/")
+
+# # Convert the response to a Python dict
+# schema = response.json()
+
+# # Dump the dict to a YAML file
+# with open("schema.yml", "w") as file:
+#     yaml.dump(schema, file)
